5408b37214e34841f01be74b246cbe310fb92bdb
Artem Potemkin <art.potemkin@gmail.com>
2022-11-22 22:42:50 +0300

    initial

---
+++ simple app/simple app/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  simple app
+//
+//  Created by Артём Потёмкин on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var firstLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hey, man! How are you? 😎"
+        label.font = .systemFont(ofSize: 25)
+        return label
+    } ()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(firstLabel)
+        setupConstarints()
+       
+       
+        
+    }
+
+    func setupConstarints () {
+        firstLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        firstLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        firstLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+    }
+
+}
+
